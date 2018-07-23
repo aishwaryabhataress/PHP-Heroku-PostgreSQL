5af9db37b8d82c1b0c7652f3b922410cb7f2a186
aishwaryabhataress <41354979+aishwaryabhataress@users.noreply.github.com>
2018-07-23 18:29:10 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = "";
-$user = "";
-$password = "";
-$dbname = "";
-$port = "";
+$host = "ec2-50-19-86-139.compute-1.amazonaws.com";
+$user = "jwxmbirkvwexoi";
+$password = "a6be799d393053f292aaf82d4a66641df5a7a28f14ec959bf373097b51bd9bf9";
+$dbname = "d3ckvlr3hl8sdn";
+$port = "5432";
 
 try{
   //Set DSN data source name
